ece20f77ea5009f94d487a97352b871b01218b21
hampus <hampus.john.olausson@gmail.com>
2021-07-07 11:15:58 +0200

    Setup done

---
+++ Integration_Testing.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Integration_Testing.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Integration_Testing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Integration_Testing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
